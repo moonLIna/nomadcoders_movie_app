3ecc49f3c9f07b1977a81d504f93f427e526a274
moonLina <63781855+moonLIna@users.noreply.github.com>
2020-05-06 17:34:40 +0900

    react key 값을 줘야하는 이유

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 function Food({name, pic}) {
-return <div>
-  <h2>I love {name}</h2>
-  <img src={pic}/>
-</div>;
+  return <div>
+    <h2>I love {name}</h2>
+    <img src={pic} alt={name}/>
+  </div>;
 }
 
 const foodILike = [
   {
+    id : 1,
     name : "water",
     image : "https://www.google.com/search?q=water&newwindow=1&safe=active&sxsrf=ALeKk00VduxSTpzfuTq-O09O30C5RUq76A:1588749535904&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi20OWd2Z7pAhWEHqYKHVIUCOcQ_AUoAXoECBwQAw&biw=1280&bih=607&dpr=1.5#imgrc=RqJdkQTA4o38fM"
   },
   {
+    id : 2,
     name : "choco",
     image : "https://vaya.in/recipes/wp-content/uploads/2018/02/Milk-Chocolate-1.jpg"
   }
@@ -24,7 +26,9 @@ function renderFood(dish){
 function App() {
   return (
     <div className="App">
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} pic={dish.image}/>
+      ))}
     </div>
   );
 }
