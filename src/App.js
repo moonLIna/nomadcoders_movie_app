140114f5f6434150456ba48bc8c1b595e7154ec5
moonLina <63781855+moonLIna@users.noreply.github.com>
2020-05-07 14:44:31 +0900

    classComponent에서 state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-
+/*
 function Food({name, pic, rating}) {
   return <div>
     <h2>I love {name}</h2>
@@ -46,6 +46,26 @@ function App() {
       ))}
     </div>
   );
+}
+*/
+
+class App extends React.Component {
+  state = {
+    count : 0
+  };
+  add = ()  => {
+    return ++this.state.count;
+  };
+  minus = ()  => {
+    console.log("Minus");
+  };
+  render(){
+      return <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+  }
 }
 
 export default App;
