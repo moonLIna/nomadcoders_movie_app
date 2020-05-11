5f8c60dd9c379874960a71fc6b9cde148e04c45a
moonLina <63781855+moonLIna@users.noreply.github.com>
2020-05-11 15:17:05 +0900

    Updates

---
+++ precache-manifest.c4371176fe06b4b7820dbf84ba3b7989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b27b7aaec73d22414ac299c53ff06ee",
+    "url": "/nomadcoders_movie_app/index.html"
+  },
+  {
+    "revision": "5d2bd350b4d67b2a4051",
+    "url": "/nomadcoders_movie_app/static/css/main.071c88e1.chunk.css"
+  },
+  {
+    "revision": "d4f39f5e062f0f3342e3",
+    "url": "/nomadcoders_movie_app/static/js/2.7c60b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomadcoders_movie_app/static/js/2.7c60b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2bd350b4d67b2a4051",
+    "url": "/nomadcoders_movie_app/static/js/main.1327e01b.chunk.js"
+  },
+  {
+    "revision": "b71c2debad8328b1ce01",
+    "url": "/nomadcoders_movie_app/static/js/runtime-main.4952058f.js"
+  }
+]);
